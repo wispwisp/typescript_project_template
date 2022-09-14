74c320308b6c7ecb39bd18d1a87a2a1825d32037
wisp <forworkandtravel@yandex.ru>
2022-09-14 10:46:21 +0000

    stage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # typescript_project_template
+
+`npm init -y`
+
+? `sudo npm install -g yarn`
+
+? `sudo chown -R $USER /usr/local/lib/node_modules`
+
+`yarn add @babel/core babel-preset-env babel-loader --dev`
+
+`yarn add webpack webpack-cli --dev`
+
+`yarn build`
+
+`yarn add html-webpack-plugin --dev --ignore-engines`
+
+`yarn add webpack-dev-server --dev --ignore-engines`
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: path.join(__dirname, 'app'),
+    entry: [
+        './js/app.js',
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        'template': path.resolve(__dirname, 'app/index.html'),
+        'inject': 'body'
+    })],
+};
